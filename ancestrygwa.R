a4c923a52a74da5c48350abf3f861ba6cc4d7f0b
Moises Exposito Alonso <moisesexpositoalonso@Moisess-MacBook-Pro.local>
2016-08-08 14:13:49 +0200

    continue working in ancestrygwa.R in the cluster

--- ancestrygwa.R
+++ ancestrygwa.R
@@ -20,38 +20,6 @@ print(sampleout[1:10,1:10])
 
 sampleout<-read.table(paste(pathresults,samplefile, sep="/") , header=F,fill=T)
 
-############labels of the painted chromoscare in omes
-
-
-labelinput<-read.table(paste( ebiopath, '/abt6_projects9/ath_1001G_image_pheno/experiment_218_droughtgwa/fineancestry/label-input-relicts.csv' ,sep=""), header=F)
-# labelinput<-read.table(paste( ebiopath, '/abt6_projects9/ath_1001G_image_pheno/experiment_218_droughtgwa/fineancestry/label-input-drought.csv' ,sep=""), header=F)
-labelinput$V4<-substring(labelinput$V2, 1, 1)
-labelinput$V5<-c(1:length(labelinput$V1))
-
-labelinput$V4=NA
-for (i in 1:length(labelinput$V2)){
-  if(nchar(as.character.factor(labelinput$V2[i])) != 1){
-  labelinput$V4[i] = substr(as.character.factor(labelinput$V2[i]), 1, nchar(as.character.factor(labelinput$V2[i]))-1)
-  }else{
-  labelinput$V4[i]=as.character.factor(labelinput$V2[i])
-  }
-
-}
-
-row.names(labelinput)=labelinput$V5
-head(labelinput)
-
-labelinput$V6<-0;labelinput$V6[which(labelinput$V4 ==11)] <-1 # is north swedish
-labelinput$V7<-0;labelinput$V7[which(labelinput$V4 ==9)] <-1 # is mediterranean
-labelinput$V8<-0;labelinput$V8[which(labelinput$V4 ==5)] <-1 # is relict
-labelinput$V9<-0;labelinput$V9[which(labelinput$V4 ==10)] <-1 # is catalan
-
-######## argument chromosome
-
-args <- commandArgs(trailingOnly = TRUE)
-chr=args[1]
-# chr=2
-
 
 
 #### load GENOME MATRIX
