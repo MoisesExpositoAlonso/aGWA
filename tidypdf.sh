f112c3796a1c2ce15595bba2cbf38903b32f5f0d
mexposito <moises.exposito-alonso@tuebingen.mpg.de>
2016-10-04 09:51:17 +0200

    removed some unnecesary files
    Former-commit-id: b09417d6ea008daa3fe61340128a308a79cdb341

--- tidypdf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#mkdir pdffiles
-#find . -maxdepth 1 -name '*.pdf' -print0 | xargs -0 -I {} "pdffiles/"
-find . -maxdepth 1 -name '*.pdf' -type f | xargs -n 1 -I '{}' mv {}  pdffiles/
-find . -maxdepth 1 -name '*.log' -type f | xargs -n 1 -I '{}' mv {}  logfiles/
-find . -maxdepth 1 -name '*gene*' -type f | xargs -n 1 -I '{}' mv {}  genefiles/
-
