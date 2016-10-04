39d7a6c516550a43936269d8bdc5721ce3de2c8a
mexposito <moises.exposito-alonso@tuebingen.mpg.de>
2016-10-04 18:39:34 +0200

    Files for example toy dataset and an example of how to run the bash file

--- create_my_labelinput.R
+++ create_my_labelinput.R
@@ -1,12 +1,12 @@
 
-accmaster<-read.table("../752_accmaster_env_str_drough.tsv",sep="\t",header = T)
+accmaster<-read.table("../762_accmaster_env_str_drough.tsv",sep="\t",header = T)
 
 wantedcolumns<-c("id","kgroup","m1d_polqua")
 label_input_genomes<-accmaster[,wantedcolumns]
 write.table(x = label_input_genomes,file = "label_input_genomes.tsv",sep="\t",quote=F,row.names=F,col.names = F)
 
-names(accmaster)
-wantedcolumns<-c("id","PC3","m1d_polqua")
-label_input_genomes<-accmaster[,wantedcolumns]
-write.table(x = label_input_genomes,file = "label_input_genomes_continuous.tsv",sep="\t",quote=F,row.names=F,col.names = F)
+#names(accmaster)
+#wantedcolumns<-c("id","PC3","m1d_polqua")
+#label_input_genomes<-accmaster[,wantedcolumns]
+#write.table(x = label_input_genomes,file = "label_input_genomes_continuous.tsv",sep="\t",quote=F,row.names=F,col.names = F)
 
---
+++ example_job.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+time bash sh-ANALYSES_ANCESTRYGWA.sh label_input_genomes.tsv discrete run_1 > run_1.log
+
